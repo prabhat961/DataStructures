74cc83a610403d57b85c8ac3a6380644208cfc11
prabhat961 <70049917+prabhat961@users.noreply.github.com>
2023-09-29 23:26:31 +0530

    Update LongestDistinctSubString.java
    modified the method

--- Strings/LongestDistinctSubString.java
+++ Strings/LongestDistinctSubString.java
@@ -3,6 +3,29 @@ package Strings;
 
 public class LongestDistinctSubString {
 
+	public statitic void longestDistinct(String str){
+		String longestSubString = null;
+		int LongestSubstinglength = 0;
+
+		map <Character, Integer>mp = new LinkedHashMap<Character, Integer>();
+		char[] arr = str.toCharArray();
+
+		for(int i = 0; i < arr.length;i++){
+			char c = arr[i];
+			if(mp.containsKey(ch)){
+				mp.put(ch, i);
+			}
+			else{
+				i = mp.get(ch);
+				mp.clear;();
+		}
+			if(mp.size() > LongestSubstinglength){
+				LongestSubstinglength = mp.size();
+				
+			longestSubString = mp.keySet().toString();	
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
